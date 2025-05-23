74dd6906e42929e98a30c3c20b6d4baffec56a30
Michael Oberegger <michael.oberegger@affinity.co>
2025-05-23 14:14:13 -0400

    Lint jbuilder_generator.rb

--- lib/generators/rails/jbuilder_generator.rb
+++ lib/generators/rails/jbuilder_generator.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails/generators/named_base'
 require 'rails/generators/resource_helpers'
 
@@ -6,7 +8,7 @@ module Rails
     class JbuilderGenerator < NamedBase # :nodoc:
       include Rails::Generators::ResourceHelpers
 
-      source_root File.expand_path('../templates', __FILE__)
+      source_root File.expand_path('templates', __dir__)
 
       argument :attributes, type: :array, default: [], banner: 'field:type field:type'
 
@@ -18,46 +20,48 @@ module Rails
       end
 
       def copy_view_files
-        %w(index show).each do |view|
+        %w[index show].each do |view|
           filename = filename_with_extensions(view)
           template filename, File.join('app/views', controller_file_path, filename)
         end
-        template filename_with_extensions('partial'), File.join('app/views', controller_file_path, filename_with_extensions("_#{singular_table_name}"))
+        template filename_with_extensions('partial'),
+                 File.join('app/views', controller_file_path,
+                           filename_with_extensions("_#{singular_table_name}"))
       end
 
-
       protected
-        def attributes_names
-          [:id] + super
+
+      def attributes_names
+        [:id] + super
+      end
+
+      def filename_with_extensions(name)
+        [name, :json, :jbuilder].join('.')
+      end
+
+      def full_attributes_list
+        if options[:timestamps]
+          attributes_list(attributes_names + %w[created_at updated_at])
+        else
+          attributes_list(attributes_names)
+        end
+      end
+
+      def attributes_list(attributes = attributes_names)
+        if self.attributes.any? { |attr| attr.name == 'password' && attr.type == :digest }
+          attributes = attributes.reject { |name| %w[password password_confirmation].include? name }
         end
 
-        def filename_with_extensions(name)
-          [name, :json, :jbuilder] * '.'
-        end
+        attributes.map { |a| ":#{a}" } * ', '
+      end
 
-        def full_attributes_list
-          if options[:timestamps]
-            attributes_list(attributes_names + %w(created_at updated_at))
-          else
-            attributes_list(attributes_names)
-          end
-        end
+      def virtual_attributes
+        attributes.select { |name| name.respond_to?(:virtual?) && name.virtual? }
+      end
 
-        def attributes_list(attributes = attributes_names)
-          if self.attributes.any? {|attr| attr.name == 'password' && attr.type == :digest}
-            attributes = attributes.reject {|name| %w(password password_confirmation).include? name}
-          end
-
-          attributes.map { |a| ":#{a}"} * ', '
-        end
-
-        def virtual_attributes
-          attributes.select {|name| name.respond_to?(:virtual?) && name.virtual? }
-        end
-
-        def partial_path_name
-          [controller_file_path, singular_table_name].join("/")
-        end
+      def partial_path_name
+        [controller_file_path, singular_table_name].join('/')
+      end
     end
   end
 end
