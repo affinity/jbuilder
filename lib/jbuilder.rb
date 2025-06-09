2bbd1b31e83a7c660d24d8669952439ae39f90b7
Michael Oberegger <michael.oberegger@affinity.co>
2025-06-09 15:37:27 -0400

    Reduce memory allocations on merge

--- lib/jbuilder.rb
+++ lib/jbuilder.rb
@@ -302,9 +302,9 @@ class Jbuilder
     elsif _blank?(current_value) || updates.nil? || current_value.empty? && ::Array === updates
       updates
     elsif ::Array === current_value && ::Array === updates
-      current_value + updates
+      current_value.concat(updates)
     elsif ::Hash === current_value && ::Hash === updates
-      current_value.deep_merge(updates)
+      current_value.deep_merge!(updates)
     else
       ::Kernel.raise MergeError.build(current_value, updates)
     end
